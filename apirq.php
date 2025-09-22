2da5932e0b5451661ae829e5baa1c58dc3ecdd68
JustanyDonkey <heitor.pineiro.s@gmail.com>
2025-09-21 21:54:31 -0300

    Update apirq.php

--- apirq.php
+++ apirq.php
@@ -1,5 +1,5 @@
-<?php 
-    date_default_timezone_set('America/Sao_Paulo');
-    require_once "api/src/routes/Roteador.php";
-    (new Roteador())->start(); // Start the router
-?>
+    <?php 
+        date_default_timezone_set('America/Sao_Paulo');
+        require_once "api/src/routes/Roteador.php";
+        (new Roteador())->start(); // Start the router
+    ?>
